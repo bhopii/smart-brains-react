1d7fe19775cb5cc044c5c62faf7efd410f3d7fff
Kar, Abhishek (ak8073) <ak8073@att.com>
2021-01-18 09:34:00 -0500

    Frontend added Facerecognition

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import Sigin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 import Particles from "react-particles-js";
 import { useState } from "react";
 import Clarifai from "clarifai";
@@ -29,6 +31,25 @@ const particlesOptions = {
 function App() {
   const [input, setInput] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  const [box, setBox] = useState({});
+  const [route, setRoute] = useState("signin");
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    return {
+      leftCol: clarifaiFace.left_col * 100,
+      topRow: clarifaiFace.top_row * 100,
+      rightCol: 100 * (1 - clarifaiFace.right_col),
+      bottomRow: 100 * (1 - clarifaiFace.bottom_row),
+    };
+  };
+
+  const displayFaceBox = (box) => {
+    console.log(box);
+    setBox(box);
+  };
 
   const onInputChange = (event) => {
     setInput(event.target.value);
@@ -36,27 +57,41 @@ function App() {
 
   const onButtonSubmit = () => {
     setImageUrl(input);
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, imageUrl).then(
-      (response) => {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      (err) => {
-        throw err;
-      }
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, imageUrl)
+      .then((response) => displayFaceBox(calculateFaceLocation(response)))
+      .catch((err) => console.error(err));
+  };
+
+  const onRouteChange = (location) => {
+    console.log("route location ============", location);
+    if (location === "signout") {
+      setIsSignedIn(false);
+    } else if (location === "home") {
+      setIsSignedIn(true);
+    }
+    setRoute(location);
   };
 
   return (
     <div className="App">
       <Particles className="particles" params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm
-        onInputChange={onInputChange}
-        onButtonSubmit={onButtonSubmit}
-      />
-      <FaceRecognition imageUrl={imageUrl} />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+      {route === "home" ? (
+        <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onButtonSubmit}
+          />
+          <FaceRecognition box={box} imageUrl={imageUrl} />
+        </div>
+      ) : route === "register" ? (
+        <Register onRouteChange={onRouteChange} />
+      ) : (
+        <Sigin onRouteChange={onRouteChange} />
+      )}
     </div>
   );
 }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,8 +1,21 @@
-const FaceRecognition = ({ imageUrl }) => {
+import "./FaceRecognition.css";
+
+const FaceRecognition = ({ imageUrl, box }) => {
+  let boxStyle = {
+    inset: `${box.topRow}% ${box.rightCol}% ${box.bottomRow}% ${box.leftCol}%`,
+  };
+  console.log(boxStyle);
   return (
     <div className="center ma">
       <div className="absolute mt2">
-        <img alt="" src={imageUrl} width="500px" height="auto"/>
+        <img
+          id="inputImage"
+          alt=""
+          src={imageUrl}
+          width="500px"
+          height="auto"
+        />
+        <div className="bounding-box" style={boxStyle}></div>
       </div>
     </div>
   );
